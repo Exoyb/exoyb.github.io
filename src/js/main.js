61d7157d2bfba1e05ed4b1cbdd67eba95f69d9e9
Exoyb <dan.ridout@icloud.com>
2026-09-10 03:00:41 +0100

    Hold authenticated SSH boot state for two more seconds

--- src/js/main.js
+++ src/js/main.js
@@ -118,7 +118,7 @@
             }, reducedMotion ? 0 : 380);
         };
 
-        window.setTimeout(hideBoot, reducedMotion ? 500 : 9600);
+        window.setTimeout(hideBoot, reducedMotion ? 500 : 11600);
         document.addEventListener('keydown', hideBoot, { once: true });
         bootScreen.addEventListener('click', hideBoot, { once: true });
     };
